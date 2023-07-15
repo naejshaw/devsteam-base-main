439a984785dc4e5dae1127a9e9a0be718727d931
naejshaw <jeanjfra@gmail.com>
2023-07-15 02:29:48 -0300

    att

---
+++ src/components/cartMenu/cartMenu.jsx
@@ -0,0 +1,31 @@
+import styles from './cartMenu.module.css'
+import CartOption from '@/components/cartOption/cartOption'
+
+export default function CartMenu({cart, onRemove}){
+    return(
+        <div className={styles.menu}>
+            <div className={styles.options}>
+                {
+                    cart.length == 0 && <p>Nenhum produto no seu carrinho</p>
+                }
+                {
+                    cart.map((cartInfo, pos) =>
+                        <CartOption
+                            image={cartInfo.image}
+                            title={cartInfo.title}
+                            price={cartInfo.pricee}
+                            onRemove={() => onRemove(pos)}
+                            key={`cart-info-${pos}`}
+                        />
+                    )
+                }
+            </div>
+            <div className={styles.priceline}>
+                <h2>Total</h2>
+                <h2 className={styles.price}>R$ {
+                    cart.reduce((prev, current) => prev + current.price, 0).toFixed(2)
+                    }</h2>
+            </div>
+        </div>
+    )
+}
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,16 +2,20 @@ import styles from './navbar.module.css'
 import { BsCart4 } from 'react-icons/bs'
 
 import Logo from '@/components/logo/logo'
-import Input from '../forms/input/input'
+import Input from '@/components/forms/input/input'
+import CartButton from '@/components/forms/cartButton/cartButton'
 
-export default function Navbar () {
+export default function Navbar ({cart, onRemove}) {
     return(
         <nav className={styles.navbar}>
             <Logo />
             <div className={styles.search}>
                 <Input type="text" placeholder="Buscar" fullWidth />
             </div>
-            <BsCart4 size={40} />
+            <CartButton 
+                cart={cart}
+                onRemove={onRemove}
+            />
         </nav>
     )
 }
